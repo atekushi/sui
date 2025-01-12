ff4b9e0c877ef00bc645ef67d2ec03e68b907ef2
Endrika Nanami <endrika.nanami@gmail.com>
2025-01-12 09:31:40 +0700

    Add phpdoc for facade class

--- src/Facades/Sui.php
+++ src/Facades/Sui.php
@@ -2,9 +2,17 @@
 
 namespace Atekushi\Sui\Facades;
 
+use Atekushi\Sui\Interfaces\ViewInterface;
 use Atekushi\Sui\Sui as BaseClass;
 use Atekushi\Support\Facade;
 
+/**
+ * @package Atekushi/Env
+ *
+ * @method static ViewInterface load(string $path, array $data = [])
+ *
+ * @mixin BaseClass
+ */
 class Sui extends Facade
 {
     protected static function getClassSubject(): string
